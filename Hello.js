8af3ae7373c676f3b7173995cf2df4ca3dffc295
BuildTools <unconfigured@null.spigotmc.org>
2024-11-29 16:56:48 +0100

    29/11

--- Hello.js
+++ Hello.js
@@ -1,5 +1,15 @@
-function Hello(name) {
-  console.log(`Hello, ${name}!`);
+function Hello() {
+  const hello = String.fromCharCode(72, 101, 108, 108, 111);
+
+  const virgule = String.fromCharCode(44);
+
+  const space = String.fromCharCode(32);
+
+  const name = String.fromCharCode(78, 97, 108, 105, 111, 115);
+
+  const exclam = String.fromCharCode(33);
+
+  console.log(hello + virgule + space + name + exclam);
 }
 
-Hello("Nalios");
+Hello();
